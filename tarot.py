7e37d892ca0ac50327bfe833a9b956076a0f096d
Intimaria <intimaria@gmail.com>
2020-09-07 22:49:11 +0000

    Add new file

---
+++ tarot.py
@@ -0,0 +1,8 @@
+import random
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+spread = {}
+spread["past"] = tarot.pop(random.randint(1, 22))
+spread["present"] = tarot.pop(random.randint(1, 22))
+spread["future"] = tarot.pop(random.randint(1, 22))
+for key,value in spread.items():
+  print("Your "+key+" is the "+value+" card.")
